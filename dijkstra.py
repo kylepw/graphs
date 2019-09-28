db86aa1cb43d0f1e7e9ac05b16c5a74cedfca560
Kyle <kylepw@gmail.com>
2019-09-28 22:56:10 +0900

    Add code

---
+++ dijkstra.py
@@ -0,0 +1,44 @@
+""" Implement Dijkstra's method to find shortest path between
+    two nodes in graph.
+"""
+import unittest
+
+
+def shortest_path(node1, node2, graph, visited=None):
+    if not node1 or not node2 or not graph or node1 not in graph or node2 not in graph:
+        return
+    if visited is None:
+        visited = []
+    graph[node1]['weight'] = 0
+
+    for node in graph:
+        for adj_node in graph[node]['adjacent']:
+            if adj_node in visited:
+                continue
+            if graph[adj_node]['weight'] > graph[node]['weight'] + graph[node]['adjacent'][adj_node]:
+                graph[adj_node]['weight'] = graph[node]['weight'] + graph[node]['adjacent'][adj_node]
+        visited.append(node)
+
+    for node in graph:
+        print(node, graph[node]['weight'])
+    return graph[node2]['weight']
+
+
+
+class TestShortestPath(unittest.TestCase):
+    def setUp(self):
+        INFINITY = 9999999
+        # https://www.youtube.com/watch?v=CL1byLngb5Q
+        self.graph = {
+            'A': {'weight': INFINITY, 'adjacent': {'B': 2, 'C': 4}},
+            'B': {'weight': INFINITY, 'adjacent': {'A': 2, 'C': 1, 'D': 4, 'E': 2}},
+            'C': {'weight': INFINITY, 'adjacent': {'A': 4, 'B': 1, 'E': 3}},
+            'D': {'weight': INFINITY, 'adjacent': {'B': 4, 'E': 3, 'F': 2}},
+            'E': {'weight': INFINITY, 'adjacent': {'B': 2, 'C': 3, 'D': 3, 'F': 2}},
+            'F': {'weight': INFINITY, 'adjacent': {'D': 2, 'E': 2}}
+        }
+    def test_shortest_path(self):
+        self.assertEqual(shortest_path('A', 'F', self.graph), 6)
+        #self.assertEqual(shortest_path('A', 'E', self.graph), 4)
+        #self.assertEqual(shortest_path('C', 'D', self.graph), 5)
+        #self.assertEqual(shortest_path('F', 'B', self.graph), 4)
---
+++ path_check.py
@@ -0,0 +1,81 @@
+import unittest
+
+
+def dfs_visited_iter(node, graph):
+    """DFS: Return visited nodes iterably via stack"""
+    stack = [node]
+    visited = []
+    while stack:
+        current = stack.pop()
+        if current not in visited:
+            visited.append(current)
+        stack.extend(graph[current])
+    return visited
+
+def dfs_visited_re(node, graph, visited=[]):
+    """DFS: Return visited nodes via recursion"""
+    for child in graph[node]:
+        if child not in visited:
+            visited.append(child)
+        dfs_visited_re(child, graph, visited)
+    return visited
+
+def bfs_visited(node, graph):
+    """BFS: Return visited nodes"""
+    queue = [node]
+    visited = []
+
+    while queue:
+        current = queue.pop()
+        # Enqueue unvisited neighbors
+        queue[0:0] = [neighbor for neighbor in graph[current] if neighbor not in visited]
+        visited.append(current)
+    return visited
+
+def is_path(node1, node2, graph, method):
+    """Return True if path between `node1` and `node2`"""
+    return node2 in method(node1, graph)
+
+
+class TestIsPath(unittest.TestCase):
+    def setUp(self):
+        self.graph1 = {
+            'A': ['B', 'D'],
+            'B': [],
+            'C': [],
+            'D': ['E'],
+            'E': ['C'],
+            'F': ['C'],
+        }
+        self.graph2 = {
+            'A': ['B', 'C', 'E'],
+            'B': ['D',],
+            'C': [],
+            'D': ['G'],
+            'E': [],
+            'F': ['E', 'C'],
+            'G': ['C'],
+        }
+
+    def test_is_path_dfs_iter(self):
+        self.assertFalse(is_path('A', 'F', self.graph1, dfs_visited_iter))
+        self.assertTrue(is_path('A', 'C', self.graph2, dfs_visited_iter))
+
+    def test_is_path_dfs_recursive(self):
+        self.assertFalse(is_path('A', 'F', self.graph1, dfs_visited_re))
+        self.assertTrue(is_path('A', 'C', self.graph2, dfs_visited_re))
+
+    def test_is_path_bfs(self):
+        self.assertFalse(is_path('A', 'F', self.graph1, bfs_visited))
+        self.assertTrue(is_path('A', 'C', self.graph2, bfs_visited))
+
+
+class TestShortestPath(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_shortest_path(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
